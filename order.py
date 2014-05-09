48da9b1171899b3630fdf3e633c25c1aeec4315b
Michael Klopot <mklopot@Michaels-MacBook-Pro.local>
2014-05-08 23:27:42 -0700

    a pretty string representation of order objects

--- order.py
+++ order.py
@@ -9,6 +9,9 @@ class Order(object):
         self.created_at = time.time()
         self.submitted_at = None
 
+    def __str__(self):
+        return "{} for user:{}, size:{}, created at:{}, submitted at:{}".format(type(self).__name__,self.user,self.size,self.created_at,self.submitted_at)
+
 class LimitOrder(Order):
     def __init__(self,user,limit,size=1):
         super(LimitOrder, self).__init__(user,size)
