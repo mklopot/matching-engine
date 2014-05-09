bb6b2d4a162c9aada8d7cc198dba337c3fb540be
Michael Klopot <mklopot@Michaels-MacBook-Pro.local>
2014-05-09 03:15:22 -0700

    Separate Buy/Sell books, and even better formatting of str()

--- order.py
+++ order.py
@@ -10,10 +10,7 @@ class Order(object):
         self.submitted_at = None
 
     def __str__(self):
-        return "{} for user:{}, size:{}, created at:{}, submitted at:{}".format(type(self).__name__,self.user,self.size,self.created_at,self.submitted_at)
-
-    def __repr__(self):
-        return "{} for user:{}, size:{}, created at:{}, submitted at:{}".format(type(self).__name__,self.user,self.size,self.created_at,self.submitted_at)
+        return "{0} submitted:{1.submitted_at} for {1.size:12.8f}  created:{1.created_at}  user:{1.user:10}".format(type(self).__name__,self)
 
 class LimitOrder(Order):
     def __init__(self,user,limit,size=1):
@@ -21,7 +18,7 @@ class LimitOrder(Order):
         self.limit = limit
 
     def __str__(self):
-        return "{} for user:{}, limit:{} size:{}, created at:{}, submitted at:{}".format(type(self).__name__,self.user,self.limit,self.size,self.created_at,self.submitted_at)
+        return "{0} at {1.limit:6.2f} for {1.size:12.8f}  created:{1.created_at}  submitted:{1.submitted_at}  user:{1.user:10}".format(type(self).__name__,self)
 
 class MarketOrder(Order):
     pass
@@ -36,33 +33,43 @@ class Orderbook(object):
             s = s + str(order) + "\n"
         return s
 
-class LimitOrderbook(Orderbook):
+class BuyLimitOrderbook(Orderbook):
     def add(self,order):
-        self.orderlist.append(order)
-        self.orderlist.sort(key=lambda k: k.limit)
-        order.submitted_at = datetime.datetime.now().strftime('%s%f')
+        if order not in self.orderlist:
+            self.orderlist.append(order)
+            self.orderlist.sort(key=lambda k: k.limit)
+            order.submitted_at = datetime.datetime.now().strftime('%s%f')
+
+class SellLimitOrderbook(Orderbook):
+    def add(self,order):
+        if order not in self.orderlist:
+            self.orderlist.append(order)
+            self.orderlist.sort(key=lambda k: k.limit, reverse=True)
+            order.submitted_at = datetime.datetime.now().strftime('%s%f')
     
 class MarketOrderbook(Orderbook):
     def add(self,order):
-        self.orderlist.append(order)
-        order.submitted_at = datetime.datetime.now().strftime('%s%f')
+        if order not in self.orderlist:
+            self.orderlist.append(order)
+            order.submitted_at = datetime.datetime.now().strftime('%s%f')
 
 
 if __name__ == "__main__":
 
     limit_order1 = LimitOrder(user="Abe",limit=10,size=7)
-    print str(limit_order1)
-    limit_order2 = LimitOrder(user="Boaz",limit=11,size=20)
+    print limit_order1
+    limit_order2 = LimitOrder(user="Boaz",limit=11.2,size=20)
     limit_order3 = LimitOrder(user="Boaz",limit=9,size=20)
 
     market_order1 = MarketOrder(user="Abe",size=3)
-    print str(market_order1)
+    print market_order1
     market_order2 = MarketOrder(user="Boaz",size=5)
-    print str(market_order2)
+    print market_order2
 
-    limit_orderbook1 = LimitOrderbook()
+    limit_orderbook1 = BuyLimitOrderbook()
     limit_orderbook1.add(limit_order1)
+    limit_orderbook1.add(limit_order2)
     limit_orderbook1.add(limit_order2)
     limit_orderbook1.add(limit_order3)
     print limit_orderbook1
-    print str(limit_order1)
+    print limit_order1
