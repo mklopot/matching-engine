df4b496a15197b287e192f603d1d3722b1c7eb97
Michael Klopot <mklopot@Michaels-MacBook-Pro.local>
2014-05-17 03:08:09 -0700

    'buy' and 'sell' commands in the shell

--- shell.py
+++ shell.py
@@ -8,8 +8,6 @@ class market_shell(cmd.Cmd):
 
     def do_user(self,args):
         tokens = shlex.split(args)
-        print tokens
-#        args = args.split(" ")
         if len(tokens) == 0:
             if not self.user:
                 print "Currently not logged in"
@@ -38,6 +36,72 @@ class market_shell(cmd.Cmd):
         print
         print "Usage: user"
         print "Display current user ID"
+
+    def auth(self):
+        if not self.user:
+            print "You must be logged in to use this function"
+            return False
+        else:
+            return True
+
+    def do_buy(self,args):
+        if self.auth():    
+            args = shlex.split(args)
+            num_args = len(args)
+            if num_args == 1:
+                try:
+                    args[0] = float(args[0])
+                except ValueError:
+                    self.help_buy()
+                    return
+                print "Placing a market BUY order for {0[0]} unit(s)".format(args)
+            elif num_args == 3 and args[1] in ["@", "limit"]:
+                try:
+                    args[0] = float(args[0])
+                    args[2] = float(args[2])
+                except ValueError:
+                    self.help_buy()
+                    return
+                print "Placing a limit BUY order for {0[0]} unit(s) at {0[2]} or better".format(args)
+            else:
+                self.help_buy()
+
+    def help_buy(self,**args):
+        print "Usage: buy <units>"
+        print "Place a BUY market order"
+        print 
+        print "Usage: buy <units> limit <limit price>"
+        print "Place a BUY limit order"
+
+    def do_sell(self,args):
+        if self.auth():
+            args = shlex.split(args)
+            num_args = len(args)
+            if num_args == 1:
+                try:
+                    args[0] = float(args[0])
+                except ValueError:
+                    self.help_sell()
+                    return
+                print "Placing a market SELL order for {0[0]} unit(s)".format(args)
+            elif num_args == 3 and args[1] in ["@", "limit"]:
+                try:
+                    args[0] = float(args[0])
+                    args[2] = float(args[2])
+                except ValueError:
+                    self.help_sell()
+                    return
+                print "Placing a limit SELL order for {0[0]} unit(s) at {0[2]} or better".format(args)
+            else:
+                self.help_sell()
+
+    def help_sell(self,**args):
+        print "Usage: sell <units>"
+        print "Place a SELL market order"
+        print 
+        print "Usage: sell <units> limit <limit price>"
+        print "Place a SELL limit order"
+
  
     def do_exit(self,args):
         print "\n"
@@ -50,6 +114,10 @@ class market_shell(cmd.Cmd):
 
     do_EOF = do_exit
     help_EOF = help_exit
+
+    def preloop(self):
+        self.do_help("")
+
 
 
 
@@ -65,6 +133,7 @@ if __name__ == "__main__":
     users = [mike]
 
     cli = market_shell()
+    cli.preloop
     cli.prompt = "> "
-    cli.cmdloop()
+    cli.cmdloop("Welcome to the Market! \nUse 'buy' and 'sell' commands to place orders.\n")
 
