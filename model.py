cbe0b4f64972304459212f919daac065082d38f2
Michael Klopot <mklopot@Michaels-MacBook-Pro.local>
2014-06-09 20:11:53 -0700

    Database model for users, transactions, and account balances

---
+++ model.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+import sqlalchemy
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = sqlalchemy.create_engine('postgresql://matching_engine:password@localhost/matching-engine')
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.String,primary_key=True,index=True)
+    password = sqlalchemy.Column(sqlalchemy.String)
+    active = sqlalchemy.Column(sqlalchemy.Boolean, default='true')
+    admin = sqlalchemy.Column(sqlalchemy.Boolean, default='false')
+
+    def __repr__(self):
+        return "<User: {0.id}>".format(self)
+
+
+class Asset(Base):
+    __tablename__ = 'assets'
+
+    name = sqlalchemy.Column(sqlalchemy.String,primary_key=True)
+
+
+class Balance(Base):
+    __tablename__ = 'balances'
+    __table_args__ = (sqlalchemy.PrimaryKeyConstraint('user','asset'),)
+
+    user = sqlalchemy.Column(sqlalchemy.String,sqlalchemy.ForeignKey('users.id'),nullable=False)
+    asset = sqlalchemy.Column(sqlalchemy.String,sqlalchemy.ForeignKey('assets.name'),nullable=False)
+    balance = sqlalchemy.Column(sqlalchemy.Float)
+
+
+class Transaction(Base):
+    __tablename__ = 'transactions'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,primary_key=True)
+    user = sqlalchemy.Column(sqlalchemy.String,sqlalchemy.ForeignKey("users.id"),nullable=False)
+    asset = sqlalchemy.Column(sqlalchemy.String,sqlalchemy.ForeignKey("assets.name"),nullable=False)
+    amount = sqlalchemy.Column(sqlalchemy.Float)
+
+
+trig_ddl = sqlalchemy.DDL("""
+    CREATE OR REPLACE FUNCTION update_balance() RETURNS trigger AS '
+    BEGIN
+    UPDATE balances SET balance = balance + NEW.amount WHERE balances.user = NEW.user AND balances.asset = NEW.asset; 
+    RETURN NEW;
+    END;'
+    LANGUAGE plpgsql;
+
+    CREATE TRIGGER update_balance_trigger BEFORE INSERT
+    ON transactions
+    FOR EACH ROW EXECUTE PROCEDURE
+    update_balance();
+""")
+
+tbl = Transaction.__table__
+sqlalchemy.event.listen(tbl, 'after_create', trig_ddl.execute_if(dialect='postgresql'))
+
+Base.metadata.create_all(engine)
+
+if __name__ == "__main__":
+    pass
